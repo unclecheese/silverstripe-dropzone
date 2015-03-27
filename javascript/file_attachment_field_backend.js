unclecheese/silverstripe-dropzone

commit dc6fbb409308d73b2530b016fb5fbf66a26fa16f

Preserve URL parameters

javascript/file_attachment_field_backend.js
@@ -6,14 +6,16 @@ $('.dropzone-holder.backend').entwine({
 		if(!dialog.length) dialog = jQuery('<div class="ss-uploadfield-dialog" id="' + dialogId + '" />');
 
 		// If user selected 'Choose another file', we need the ID of the file to replace
-		var iframeUrl = config['urlSelectDialog'];
+		var iParams = config['urlSelectDialog'].indexOf('?'),
+		    iframeUrl = config['urlSelectDialog'].substr(0, iParams),
+		    iframeParams = config['urlSelectDialog'].substr(iParams+1);
 		var uploadedFileId = null;
 		if (uploadedFile && uploadedFile.attr('data-fileid') > 0){
 			uploadedFileId = uploadedFile.attr('data-fileid');
 		}
 
 		// Show dialog
-		dialog.ssdialog({iframeUrl: iframeUrl, height: 550});
+		dialog.ssdialog({iframeUrl: iframeUrl + '?' + iframeParams, height: 550});
 
 		// TODO Allow single-select
 		dialog.find('iframe').bind('load', function(e) {
@@ -31,7 +33,7 @@ $('.dropzone-holder.backend').entwine({
 				var ids = $.map(gridField.find('.ss-gridfield-item.ui-selected'), function(el) {return $(el).data('id');});
 				if(ids && ids.length) {
 					$.ajax({
-						url: iframeUrl+'/filesbyid?ids='+ids.join(','),
+						url: iframeUrl+'/filesbyid?ids='+ids.join(',') + '&' + iframeParams,
 						dataType: 'JSON',
 						success: function (json) {
 							json.forEach(function(item) {

commit 1e71fcaac8259356744679e399b452a0d9419021

BUGFIX: Question mark prepended on urlSelectDialog
Regression caused by dc6fbb409308d73b2530b016fb5fbf66a26fa16f

javascript/file_attachment_field_backend.js
@@ -4,16 +4,14 @@ $('.dropzone-holder.backend').entwine({
 		// Create dialog and load iframe
 		var uploadedFile, self = this, config = this.data('config'), dialogId = 'ss-uploadfield-dialog-' + this.attr('id'), dialog = jQuery('#' + dialogId);
 		if(!dialog.length) dialog = jQuery('<div class="ss-uploadfield-dialog" id="' + dialogId + '" />');
-
 		// If user selected 'Choose another file', we need the ID of the file to replace
-		var iParams = config['urlSelectDialog'].indexOf('?'),
-		    iframeUrl = config['urlSelectDialog'].substr(0, iParams),
+		var iParams = config['urlSelectDialog'].indexOf('?'), i
+		    iframeUrl = iParams > -1 ? config['urlSelectDialog'].substr(0, iParams) : config['urlSelectDialog'];
 		    iframeParams = config['urlSelectDialog'].substr(iParams+1);
 		var uploadedFileId = null;
 		if (uploadedFile && uploadedFile.attr('data-fileid') > 0){
 			uploadedFileId = uploadedFile.attr('data-fileid');
 		}
-
 		// Show dialog
 		dialog.ssdialog({iframeUrl: iframeUrl + '?' + iframeParams, height: 550});
 