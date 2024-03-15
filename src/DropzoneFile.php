5b844874b42d601e822a52ecdce0c8128f41bea9
chromos33 <chromos33@users.noreply.github.com>
2024-03-15 15:47:21 +0100

    Update DropzoneFile.php

--- src/DropzoneFile.php
+++ src/DropzoneFile.php
@@ -84,7 +84,7 @@ class DropzoneFile extends DataExtension
     protected function getFilenameForType($ext, $size)
     {
         return ModuleResourceLoader::singleton()->resolveURL(sprintf(
-            'unclecheese/dropzone:images/file-icons/%spx/%s.png',
+            'chromos33/dropzone:images/file-icons/%spx/%s.png',
             $size,
             strtolower($ext)
         ));
